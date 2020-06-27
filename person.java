da1286bfaf6f30cfa92f7e4dc9cecd746c5b1bd0
IbrahimBazenga <60087382+IbrahimBazenga@users.noreply.github.com>
2020-06-28 00:23:50 +0300

    Add files via upload

---
+++ person.java
@@ -0,0 +1,33 @@
+  public class Person {
+
+private  String name;
+
+// Getter method takes the value of the variable name
+
+public String  getName() {
+return name;
+
+//setter takes parameter(newName) and assings it to the name variable.
+}
+public void setName(String newName) {
+
+this.name=newName;
+
+}
+
+}
+
+  class MyClass {
+
+ public static void main(String[] args) {
+
+person ibraz=new person();
+
+ibraz.setName("ibrahim"); //it sets the value of variable name to ibrahim.
+
+System.out.println(ibraz.getName());
+  }
+
+  } 
+
+
